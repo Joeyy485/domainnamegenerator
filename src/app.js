23964467d4d98a51f89a0f521cdfc895f04b6d6d
Joeyy485 <joeypineda@live.com>
2023-04-13 00:34:39 +0000

    Updated app.js files and index.html files

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const adj = ["great", "cool", "amazing", "incredible", "wonderful"];
+const pronouns = ["you", "we", "they", "them", "she"];
+const nouns = ["website", "project", "company", "business", "idea"];
+const ext = [".com", ".net", ".org"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const a = Math.floor(Math.random() * adj.length);
+const b = Math.floor(Math.random() * pronouns.length);
+const c = Math.floor(Math.random() * nouns.length);
+const extension = ext[Math.floor(Math.random() * ext.length)];
+const domainName = adj[a] + pronouns[b] + nouns[c] + extension;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log("www." + domainName);
