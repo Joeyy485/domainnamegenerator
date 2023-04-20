343ea074210e07a852575ba61f57b3ed655cc523
Joeyy485 <joeypineda@live.com>
2023-04-20 01:11:49 +0000

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ const pronouns = ["you", "we", "they", "them", "she"];
 const nouns = ["website", "project", "company", "business", "idea"];
 const ext = [".com", ".net", ".org"];
 
-const a = Math.floor(Math.random() * adj.length);
-const b = Math.floor(Math.random() * pronouns.length);
-const c = Math.floor(Math.random() * nouns.length);
-const extension = ext[Math.floor(Math.random() * ext.length)];
-const domainName = adj[a] + pronouns[b] + nouns[c] + extension;
-
-console.log("www." + domainName);
+for (let a = 0; a < adj.length; a++) {
+    for (let b = 0; b < pronouns.length; b++) {
+      for (let c = 0; c < nouns.length; c++) {
+        for (let d = 0; d < ext.length; d++) {
+          const domainName = "www." + adj[a] + pronouns[b] + nouns[c] + ext[d];
+          console.log(domainName);
+        }
+      }
+    }
+  }
